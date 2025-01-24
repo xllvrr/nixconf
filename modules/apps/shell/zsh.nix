d549bb892286928e7b87eb69f708a9a4aa82eed9
xllvrr <dan@dtsa.email>
2025-01-24 10:16:37 +0800

    Added function to vertically center zsh prompt

--- modules/apps/shell/zsh.nix
+++ modules/apps/shell/zsh.nix
@@ -30,8 +30,15 @@
                 man $1 | col -bx | bat -l man -p
             }
 
-            # For centering the screen
-            PS1=$'\n\n\n\n\n\n\n\n\e[8A'"$PS1"
+            # Trying out centering
+            precmd_center() {
+              local CENTER=$(( LINES / 2))
+              # print as many lines necessary so that content moves upward past half-screen
+              printf '%.0s\n' {1..$(( LINES - CENTER ))}
+              # move the cursor upward as many lines where printed to put it back where it was
+              tput cuu $(( LINES - CENTER ))
+            }
+            add-zsh-hook precmd precmd_center
 
         '';
     };
