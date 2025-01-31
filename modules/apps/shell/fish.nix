1b705f900bd322f8d22294932ae285845df34526
xllvrr <dan@dtsa.email>
2025-01-31 09:07:07 +0800

    Colored man pages and vi mode for fish shell

--- modules/apps/shell/fish.nix
+++ modules/apps/shell/fish.nix
@@ -7,9 +7,13 @@
         enable = true;
         preferAbbrs = true;
         functions = {
-            batman = "man $1 | col -bx | bat -l man -p";
+            batman = "man $argv | col -bx | bat -l man -p";
         };
         shellInit = "${pkgs.openssh}/bin/ssh-add ~/.ssh/github_rsa";
+        interactiveShellInit = ''
+            # Set Vi Mode
+            set -g fish_key_bindings fish_vi_key_bindings
+        '';
     };
 
 }
