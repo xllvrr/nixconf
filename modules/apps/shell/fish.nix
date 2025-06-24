5d5e596ffbaa49e1e0b9e02020f0191c6a8d2c30
xllvrr <dan@dtsa.email>
2025-06-24 13:02:32 +0800

    Added yazi shortcut

--- modules/apps/shell/fish.nix
+++ modules/apps/shell/fish.nix
@@ -1,23 +1,27 @@
-{ pkgs, ... }:
-
-{
-
-    # Fish
-    programs.fish = {
-        enable = true;
-        preferAbbrs = true;
-        functions = {
-            batman = "man $argv | col -bx | bat -l man -p";
-        };
-        shellAbbrs = {
-            n = "nnn";
-            lg = "lazygit";
-        };
-        shellInit = "${pkgs.openssh}/bin/ssh-add ~/.ssh/github_rsa";
-        interactiveShellInit = ''
-            # Set Vi Mode
-            set -g fish_key_bindings fish_vi_key_bindings
-        '';
+{pkgs, ...}: {
+  # Fish
+  programs.fish = {
+    enable = true;
+    preferAbbrs = true;
+    functions = {
+      batman = "man $argv | col -bx | bat -l man -p";
+      y = ''
+                   set tmp (mktemp -t "yazi-cwd.XXXXXX")
+                yazi $argv --cwd-file="$tmp"
+                if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        	builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+      '';
     };
-
+    shellAbbrs = {
+      n = "nnn";
+      lg = "lazygit";
+    };
+    shellInit = "${pkgs.openssh}/bin/ssh-add ~/.ssh/github_rsa";
+    interactiveShellInit = ''
+      # Set Vi Mode
+      set -g fish_key_bindings fish_vi_key_bindings
+    '';
+  };
 }
