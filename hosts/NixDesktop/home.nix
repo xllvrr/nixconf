0a9c010ec70bc16989126e7e37fa787b707723c3
xllvrr <dtsa.tan@gmail.com>
2025-08-15 14:48:03 +0800

    Changed nixconf path

--- hosts/NixDesktop/home.nix
+++ hosts/NixDesktop/home.nix
@@ -99,7 +99,7 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 5d --keep 5";
-    flake = "/home/xllvr/nixos";
+    flake = "/home/xllvr/nixconf";
   };
 
   # Mako settings
