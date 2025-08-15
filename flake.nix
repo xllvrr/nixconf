1dd2469cf8f23104f9408fe4abdec03774cbaa24
xllvrr <dtsa.tan@gmail.com>
2025-08-15 17:51:16 +0800

    Changed home manager url in flake to stable release

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     nvf.url = "github:notashelf/nvf";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
