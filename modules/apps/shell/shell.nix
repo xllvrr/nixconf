5e31ffa0ccce19bd8ec255e8cfbfbad17bb49a58
xllvrr <dtsa.tan@gmail.com>
2026-02-26 22:54:35 +0800

    Added mouse support and set index to 1 for tmux

--- modules/apps/shell/shell.nix
+++ modules/apps/shell/shell.nix
@@ -71,7 +71,9 @@
     terminal = "screen-256color";
     prefix = "C-a";
     sensibleOnTop = true;
+    baseIndex = 1;
     keyMode = "vi";
+    mouse = true;
     plugins = with pkgs; [
       tmuxPlugins.sidebar
       tmuxPlugins.tmux-fzf
