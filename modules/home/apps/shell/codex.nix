c0edc88a8dbf3e69c886c3f6d501af844327e07b
xllvrr <dtsa.tan@gmail.com>
2026-05-23 08:59:46 +0800

    Added MCPs for Codex

--- modules/home/apps/shell/codex.nix
+++ modules/home/apps/shell/codex.nix
@@ -1,5 +1,24 @@
 {pkgs, ...}: {
+  home.packages = with pkgs; [
+    nodejs
+    mcp-nixos
+    docker
+  ];
+
   programs.codex = {
     enable = true;
+
+    settings = {
+      mcp_servers = {
+        context7 = {
+          command = "npx";
+          args = ["-y" "@upstash/context7-mcp"];
+        };
+
+        nixos = {
+          command = "mcp-nixos";
+        };
+      };
+    };
   };
 }
