9354f9265468fa3fc79e0d8992ad895df60843e8
xllvrr <dtsa.tan@gmail.com>
2026-05-23 11:14:21 +0800

    Deleted some extraneous stuff for chromium

--- modules/home/apps/browser/chromium.nix
+++ modules/home/apps/browser/chromium.nix
@@ -20,22 +20,4 @@
       {id = "hfjbmagddngcpeloejdejnfgbamkjaeg";}
     ];
   };
-
-  # Some launchers/docks look up icons by desktop-file id (e.g. `chromium-browser`)
-  # instead of honoring `Icon=chromium`. Provide an alias icon name.
-  xdg.dataFile = let
-    iconSizes = [
-      "16x16"
-      "24x24"
-      "48x48"
-      "64x64"
-      "128x128"
-      "256x256"
-    ];
-    mkIcon = size: {
-      name = "icons/hicolor/${size}/apps/chromium-browser.png";
-      value.source = "${pkgs.chromium}/share/icons/hicolor/${size}/apps/chromium.png";
-    };
-  in
-    builtins.listToAttrs (map mkIcon iconSizes);
 }
