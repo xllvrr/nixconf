01e3d8692e8fd911a9a6929f7a654a0d49187de7
xllvrr <dtsa.tan@gmail.com>
2026-06-04 10:10:11 +0800

    Added Ghostery for adblocking

--- modules/home/apps/browser/chromium.nix
+++ modules/home/apps/browser/chromium.nix
@@ -1,22 +1,23 @@
 # Chromium Setup (mirrors Firefox essentials)
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.chromium = {
     enable = true;
 
     # Match Firefox extensions:
     # - Bitwarden
-    # - uBlock Origin
+    # - Ghostery
     # - Dark Reader
     # - Vimium C
     # - ShopBack
     # - Shopee Price Tracker (AliPrice)
     extensions = [
-      {id = "nngceckbapebfimnlniiiahkandclblb";}
-      {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";}
-      {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";}
-      {id = "hfjbmagddngcpeloejdejnfgbamkjaeg";}
-      {id = "djjjmdgomejlopjnccoejdhgjmiappap";}
-      {id = "oanlehpljgeknlohgbakodejdbingjpj";}
+      { id = "nngceckbapebfimnlniiiahkandclblb"; }
+      { id = "mlomiejdfkolichcflejclcbmpeaniij"; }
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }
+      { id = "hfjbmagddngcpeloejdejnfgbamkjaeg"; }
+      { id = "djjjmdgomejlopjnccoejdhgjmiappap"; }
+      { id = "oanlehpljgeknlohgbakodejdbingjpj"; }
     ];
   };
 }
