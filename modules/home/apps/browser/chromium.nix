94cc9ba1da3dfd19abf878d5a78e398708316fb5
xllvrr <dtsa.tan@gmail.com>
2026-09-04 13:51:08 +0800

    Removed Ali Price Tracker from Chromium

--- modules/home/apps/browser/chromium.nix
+++ modules/home/apps/browser/chromium.nix
@@ -1,23 +1,21 @@
 # Chromium Setup (mirrors Firefox essentials)
 { pkgs, ... }:
 {
-  programs.chromium = {
-    enable = true;
+    programs.chromium = {
+        enable = true;
 
-    # Match Firefox extensions:
-    # - Bitwarden
-    # - Ghostery
-    # - Dark Reader
-    # - Vimium C
-    # - ShopBack
-    # - Shopee Price Tracker (AliPrice)
-    extensions = [
-      { id = "nngceckbapebfimnlniiiahkandclblb"; }
-      { id = "mlomiejdfkolichcflejclcbmpeaniij"; }
-      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }
-      { id = "hfjbmagddngcpeloejdejnfgbamkjaeg"; }
-      { id = "djjjmdgomejlopjnccoejdhgjmiappap"; }
-      { id = "oanlehpljgeknlohgbakodejdbingjpj"; }
-    ];
-  };
+        # Match Firefox extensions:
+        # - Bitwarden
+        # - Ghostery
+        # - Dark Reader
+        # - Vimium C
+        # - ShopBack
+        extensions = [
+            { id = "nngceckbapebfimnlniiiahkandclblb"; }
+            { id = "mlomiejdfkolichcflejclcbmpeaniij"; }
+            { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }
+            { id = "hfjbmagddngcpeloejdejnfgbamkjaeg"; }
+            { id = "djjjmdgomejlopjnccoejdhgjmiappap"; }
+        ];
+    };
 }
