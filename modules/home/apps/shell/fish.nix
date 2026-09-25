b57d9488a79f231ec57a17fb45d8fc9e443b4be7
xllvrr <dtsa.tan@gmail.com>
2026-09-25 12:02:06 +0800

    Update: Created a single source of truth for SSH configuration and removed from fish and niri

--- modules/home/apps/shell/fish.nix
+++ modules/home/apps/shell/fish.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   # Fish
   programs.fish = {
@@ -8,12 +8,10 @@
       batman = "man $argv | col -bx | bat -l man -p";
     };
     shellAbbrs = {
-      n = "nnn";
       lg = "lazygit";
       cd = "z";
       bt = "but-cli";
     };
-    shellInit = "${pkgs.openssh}/bin/ssh-add ~/.ssh/github_key";
     interactiveShellInit = ''
       # Set Vi Mode
       set -g fish_key_bindings fish_vi_key_bindings
--- modules/home/apps/shell/ssh.nix
+++ modules/home/apps/shell/ssh.nix
@@ -1,4 +1,8 @@
-{...}: {
+{ config, ... }:
+let
+  sshDir = "${config.home.homeDirectory}/.ssh";
+in
+{
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
@@ -10,20 +14,17 @@
         ServerAliveInterval = 0;
         ServerAliveCountMax = 3;
         HashKnownHosts = false;
-        UserKnownHostsFile = "~/.ssh/known_hosts";
+        UserKnownHostsFile = "${sshDir}/known_hosts";
         ControlMaster = "no";
-        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPath = "${sshDir}/master-%r@%n:%p";
         ControlPersist = "no";
       };
-      github = {
-        HostName = "dan@dtsa.email";
-        IdentityFile = "/home/xllvr/.ssh/github_rsa";
+      "github.com" = {
+        HostName = "github.com";
+        User = "git";
+        IdentityFile = "${sshDir}/github_key";
+        IdentitiesOnly = true;
         AddKeysToAgent = "yes";
-        ForwardAgent = false;
-        Compression = false;
-        HashKnownHosts = false;
-        UserKnownHostsFile = "~/.ssh/known_hosts";
-        ControlMaster = "no";
       };
     };
   };
